bbd6217badee4f8c2ab84a7800d6d6ed431a3a67
xianlang <1553093290@qq.com>
2024-07-10 18:04:24 +0800

    feat: 添加依赖，测试md界面渲染

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [vue(), mdx()],
+});
