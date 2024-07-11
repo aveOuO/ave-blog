405fa67d9ee8497ee76e7aa3cbcf26ffa62ce723
xianlang <1553093290@qq.com>
2024-07-11 11:36:17 +0800

    feat: 测试elementplus组件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import mdx from '@astrojs/mdx';
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [vue(), mdx()],
+    vite: {
+        plugins: [
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            })
+        ],
+        ssr: {
+            noExternal: ['element-plus'],
+        }
+    }
 });
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="aaaa">
+    组件测试：
+    <el-button type="primary">测试按钮</el-button>
+    <el-button type="warning">警告</el-button>
+    <el-button type="danger">危险</el-button>
+    <el-button type="info">信息</el-button>
+    <el-button>普通</el-button>
+  </div>
+</template>
+
+<script setup name="TestComponent" lang="ts"></script>
+
+<style scoped lang="scss">
+.aaaa {
+  display: flex;
+  justify-content: center;
+  .el-button {
+    font-size: 28px;
+  }
+}
+</style>
