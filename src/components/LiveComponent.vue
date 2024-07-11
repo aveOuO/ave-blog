6e0f08af2a2b6ae0499aa66694edbb9dfb1c9df2
xianlang <1553093290@qq.com>
2024-07-11 17:59:00 +0800

    feat: 修复组件导入失败的问题

--- src/components/LiveComponent.vue
+++ src/components/LiveComponent.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <!-- <JsScript src="../src/assets/live2d-widget-0.9.0/autoload.js"></JsScript> -->
-    <JsScript src="../src/assets/live2d-widget-0.9.0/autoload.js"></JsScript>
+    <js-script
+      src="https://cdn.jsdelivr.net/gh/aveOuO/ave-blog@main/src/assets/live2d-widget-0.9.0/autoload.js"
+    ></js-script>
   </div>
 </template>
 
 <script setup lang="ts">
 const JsScript = defineComponent({
   name: "JsScript",
-  render: (props: { src: string }) =>
-    h("script", { type: "text/javascript", src: props.src }),
+  props: {
+    src: String,
+  },
+  setup: (props) => {
+    return () => h("script", { type: "text/javascript", src: props.src });
+  },
 });
 
 const CssStyle = {
